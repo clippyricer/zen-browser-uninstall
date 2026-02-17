173d7d2ddf9a6b5a0db9b9314075a8da0fe409be
clippyricer <bannansmooth68@outlook.com>
2026-02-17 18:57:03 -0500

    add $HOME

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Uninstall zen fully script
 
-rm -rf ~/.tarball-installations/zen*
-rm -rf ~/.zen/
-rm -rf ~/.local/bin/zen*
-rm -rf ~/.local/share/applications/zen*
-rm -rf ~/.config/zen/
-rm -rf ~/.cache/zen/
+rm -rf $HOME/.tarball-installations/zen*
+rm -rf $HOME/.zen/
+rm -rf $HOME/.local/bin/zen*
+rm -rf $HOME/.local/share/applications/zen*
+rm -rf $HOME/.config/zen/
+rm -rf $HOME/.cache/zen/
 
